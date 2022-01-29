bd44a1b0af1e740b02c559baf8c96f713c400207
Ink33 <Ink33@smlk.org>
2022-01-29 15:18:14 +0800

    refator: deprecated SunandMoon API

--- v7/astronomy.go
+++ v7/astronomy.go
@@ -2,6 +2,7 @@ package v7
 
 import "time"
 
+// Deprecated: 该API已被弃用拆分, 请使用 NewSunClient 和 NewMoonClient
 // NewSunandMoonClient 创建一个日出日落、月升月落和月相查询实例。
 // https://dev.heweather.com/docs/api/astronomy
 func NewSunandMoonClient(location, date string) (client HeWeatherAPI) {
@@ -26,7 +27,7 @@ func NewSunClient(location, date string) (client HeWeatherAPI) {
 	}
 }
 
-// NewmoonClient 创建一个月升月落和月相查询实例。
+// NewMoonClient 创建一个月升月落和月相查询实例。
 // https://dev.qweather.com/docs/api/astronomy/moon-and-moon-phase/
 func NewMoonClient(location, date string) (client HeWeatherAPI) {
 	return &universeHeWeatherAPI{
@@ -46,6 +47,18 @@ func NewSolarElevationAngleClient(location, date, timeStr, tz, alt string) (clie
 		Name:      "astronomy",
 		Parameter: map[string]string{"date": date, "location": location, "time": timeStr, "tz": tz, "alt": alt},
 		SubName:   "solar_elevation_angle",
+		Timeout:   15 * time.Second,
+	}
+}
+
+// NewSolarRadiationClient 创建一个太阳辐射查询实例。
+// https://dev.qweather.com/docs/api/astronomy/solar-radiation-hourly-forecast/
+func NewSolarRadiationClient(location, duration string) (client HeWeatherAPI) {
+	return &universeHeWeatherAPI{
+		isGeo:     false,
+		Name:      "astronomy",
+		Parameter: map[string]string{"duration": duration, "location": location},
+		SubName:   "solar_radiation",
 		Timeout:   15 * time.Second,
 	}
 }
